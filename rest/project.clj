2ae8a21d4ea15e40c43c142fd530163ea307972b
HARVEE CHEN <harvee.chen@calix.com>
2018-01-16 21:03:39 +0800

    v1.0.0

--- rest/project.clj
+++ rest/project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [c3p0/c3p0 "0.9.1.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler rest.handler/app}
   :profiles
--- rest/src/rest/handler.clj
+++ rest/src/rest/handler.clj
@@ -1,11 +1,71 @@
 (ns rest.handler
-  (:require [compojure.core :refer :all]
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource)
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.json :as middleware]
+            [clojure.java.jdbc :as sql]))
+
+(def db-spec 
+  {:classname "com.mysql.jdbc.Driver"
+   :subprotocol "mysql"
+   :subname "//localhost:3306/db"
+   :user "itxia"
+   :password "itxia"})
+
+(defn pool [spec]
+  (let [cpds (doto (ComboPooledDataSource.)
+    (.setDriverClass (:classname spec))
+    (.setJdbcUrl (str "jdbc:" (:subprotocol spec) ":" (:subname spec)))
+    (.setUser (:user spec))
+    (.setPassword (:password spec))
+    ;; expire excess connections after 30 minutes of inactivity:
+    (.setMaxIdleTimeExcessConnections (* 30 60))
+    ;; expire connections after 3 hours of inactivity:
+    (.setMaxIdleTime (* 3 60 60)))]
+    {:datasource cpds}))
+
+(def pooled-db (delay (pool db-spec)))
+
+(defn db-connection [] @pooled-db)
+
+(defn create-users []
+   (sql/create-table
+    :users
+    [:id :integer "PRIMARY KEY" "AUTO_INCREMENT"]
+    [:fname "varchar(25)"]
+    [:lname "varchar(25)"]))
+
+(defn drop-users []
+   (sql/drop-table :users))
+
+(sql/with-connection (db-connection)
+  (drop-users)
+  (create-users))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn get-user [id]
+  (sql/with-connection (db-connection)
+    (sql/with-query-results results
+      ["select * from users where id = ?" id]
+      (cond 
+        (empty? results) {:status 404}
+        :else (response (first results))))))
+
+(defn create-new-user [body]
+  (let [id (uuid)]
+    (sql/with-connection (db-connection)
+      (let [user (assoc body "id" id)]
+        (sql/insert-record :users user)))
+    (get-user id)))
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/" {body :body} (create-new-user body))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)))
